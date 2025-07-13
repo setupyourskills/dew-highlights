ad5f502f6b466c8138ab815659f5944de8556ef9
Thubten Rigzin <thubtenrigzin@gmail.com>
2025-07-13 11:23:49 +0800

    feat: add initial Dew Highlights module

---
+++ lua/neorg/modules/external/dew-highlights/module.lua
@@ -0,0 +1,34 @@
+local neorg = require "neorg.core"
+local modules = neorg.modules
+
+local api = vim.api
+
+local module = modules.create "external.dew-highlights"
+
+module.load = function()
+  module.private.set_autocmd()
+end
+
+module.config.public = {
+  highlights_table = {},
+}
+
+module.private = {
+  apply_highlights = function()
+    local hl_table = module.config.public.highlights_table or {}
+    for group, opts in pairs(hl_table) do
+      api.nvim_set_hl(0, group, opts)
+    end
+  end,
+
+  set_autocmd = function()
+    api.nvim_create_autocmd({ "FileType" }, {
+      pattern = "norg",
+      callback = function()
+        module.private.apply_highlights()
+      end,
+    })
+  end,
+}
+
+return module
