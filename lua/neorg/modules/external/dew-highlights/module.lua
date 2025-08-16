3397d7c6f3b23952745c72d0b3aa9e4ba5e1d493
Thubten Rigzin <thubtenrigzin@gmail.com>
2025-08-16 13:09:03 +0800

    feat(table): auto-populate default values when not configured by the user

--- lua/neorg/modules/external/dew-highlights/module.lua
+++ lua/neorg/modules/external/dew-highlights/module.lua
@@ -8,13 +8,31 @@ local module = modules.create "external.dew-highlights"
 
 module.load = function()
   module.private.set_autocmd()
+
+  module.private.set_default_highlights()
 end
 
 module.config.public = {
   highlights_table = {},
 }
 
+module.config.private = {
+  default_highlights_table = {
+    ["@neorg.markup.verbatim"] = { fg = "#ffab00", italic = true },
+    ["dewTransclude"] = { fg = "#D2DAD7", bg = "#2A3230" },
+  },
+}
+
 module.private = {
+  set_default_highlights = function()
+    module.config.public.highlights_table =
+      vim.tbl_extend(
+        "keep",
+        module.config.public.highlights_table,
+        module.config.private.default_highlights_table
+      )
+  end,
+
   apply_highlights = function()
     local hl_table = module.config.public.highlights_table or {}
     for group, opts in pairs(hl_table) do
